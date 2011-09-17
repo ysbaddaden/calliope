e5354bf94748d3289504275fc9764bc60b1fe979
Julien Portalier <ysbaddaden@gmail.com>
2011-09-17 19:04:07 +0200

    abstraction of @blog source in BlogsController

--- lib/calliope/controllers/blogs_controller.rb
+++ lib/calliope/controllers/blogs_controller.rb
@@ -1,14 +1,18 @@
 module Calliope
   class BlogsController < ApplicationController
     def edit
-      @blog = Blog.find_by_name(params[:name])
+      @blog = blog
       respond_with(@blog)
     end
 
     def update
-      @blog = Blog.find_by_name(params[:name])
+      @blog = blog
       @blog.update_attributes(params[:blog])
       respond_with(@blog, :location => root_blog_url(params[:name]))
+    end
+
+    def blog
+      @blog ||= Blog.find_by_name(params[:name])
     end
   end
 end
